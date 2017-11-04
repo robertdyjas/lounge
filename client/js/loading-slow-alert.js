51d8a806e8538c5390c01139cfd0981d5b142006
Pavel Djundik <github@xpaw.me>
2017-11-04 19:07:41 +0200

    Handle JS errors

--- client/js/loading-slow-alert.js
+++ client/js/loading-slow-alert.js
@@ -19,3 +19,16 @@ setTimeout(function() {
 document.getElementById("loading-slow-reload").addEventListener("click", function() {
 	location.reload();
 });
+
+if ("ErrorEvent" in window) {
+	window.addEventListener("error", function(error) {
+		console.error(error);
+
+		var element = document.createElement("pre");
+		element.textContent = error.message + error.error.stack;
+
+		var title = document.getElementById("loading-title");
+		title.textContent = "An error has occured";
+		title.parentNode.appendChild(element);
+	});
+}
